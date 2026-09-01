6ede2b6615e272ea4bc4e223b82b509c0598e666
Ahamed Shafki <shafkiahamed2005@gmail.com>
2026-09-01 23:30:14 +0530

    Add initial implementation for marks input and average calculation

---
+++ new project.py
@@ -0,0 +1,16 @@
+# get marks from the user 
+mark1 = float(input("Enter mark 1 :"))
+mark2 = float(input("Enter mark 2 :"))
+mark3 = float(input("Enter mark 3 :"))
+type("mark1")
+
+# calculate the total
+total = mark1 + mark2 + mark3
+
+# calculate the average 
+average = total / 3
+
+# display the output
+print("Total =", total)
+print("Average =", average)
+
